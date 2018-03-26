40ca45bf6105ce263ef76c806b8172e8673f9e1f
Sergey Kovalevich <inndie@gmail.com>
2018-03-26 13:27:00 +0300

    Improved performance of reading files

--- include/netbox/pcap/reader.hpp
+++ include/netbox/pcap/reader.hpp
@@ -6,6 +6,7 @@
 #define KSERGEY_reader_160318101433
 
 #include <cstdio>
+#include <cstring>
 #include <vector>
 #include <utility>
 #include "../byte_order.hpp"
@@ -27,6 +28,8 @@ private:
     Scale scale_{PassThrough};
     int timezone_{0};
     std::vector< char > buffer_;
+    char* begin_{nullptr};
+    char* end_{nullptr};
 
 public:
     Reader(const Reader&) = delete;
@@ -34,7 +37,8 @@ public:
 
     /// Construct reader
     /// @param[in] filename is path to PCAP file
-    Reader(const char* filename);
+    /// @param[in] bufferSize is buffer size for i/o ops
+    Reader(const char* filename, std::size_t bufferSize = 16 * 1024 * 1024);
 
     /// Return true if file valid
     explicit operator bool() const noexcept;
@@ -50,12 +54,16 @@ public:
 
 private:
     void readFileHeader();
+    void fillBuffer();
+    std::size_t read(void*& buffer, std::size_t size);
 };
 
-inline Reader::Reader(const char* filename)
+inline Reader::Reader(const char* filename, std::size_t bufferSize)
     : file_{filename}
+    , buffer_(bufferSize)
 {
     readFileHeader();
+    fillBuffer();
 }
 
 inline Reader::operator bool() const noexcept
@@ -75,16 +83,15 @@ inline bool Reader::eof() const noexcept
 
 inline Packet Reader::readPacket()
 {
-    PacketHeader header;
-
-    // Read packet header
-    if (std::uint32_t count = file_.readStruct(header); count != sizeof(header)) {
+    void* storage;
+    if (std::uint32_t count = read(storage, sizeof(PacketHeader)); count != sizeof(PacketHeader)) {
         if (count != 0) {
-            debug("<WARN> Header read %u of %u", count, sizeof(header));
+            debug("<WARN> Header read %u of %u", count, sizeof(PacketHeader));
         }
         return {};
     }
 
+    PacketHeader& header = *reinterpret_cast< PacketHeader* >(storage);
     switch (scale_) {
         case Up:
             header.ts_usec *= 1000;
@@ -94,17 +101,17 @@ inline Packet Reader::readPacket()
             break;
     }
 
-    if (header.caplen > buffer_.size()) {
-        debug("<WARN> PCAP packet header caplen(%u) greater buffer size", header.caplen);
+    if (header.caplen > MaxSnapLen) {
+        debug("<WARN> PCAP packet header caplen(%u) greater MaxSnapLen(%u)", header.caplen, MaxSnapLen);
         return {};
     }
 
-    if (std::uint32_t count = file_.read(buffer_.data(), header.caplen); count != header.caplen) {
+    if (std::uint32_t count = read(storage, header.caplen); count != header.caplen) {
         debug("<WARN> Data read %u of %u", count, header.caplen);
         return {};
     }
 
-    return {header, buffer_.data()};
+    return {header, storage};
 }
 
 inline void Reader::readFileHeader()
@@ -138,8 +145,6 @@ inline void Reader::readFileHeader()
                 header.snaplen, MaxSnapLen);
     }
 
-    debug("Snaplen %u", header.snaplen);
-
     if (header.linktype != Ethernet) {
         return (file_ = {}), debug("<WARN> Linktype %u not supported", header.linktype);
     }
@@ -147,12 +152,40 @@ inline void Reader::readFileHeader()
     // Timezone correction
     timezone_ = header.thiszone;
 
+#if 0
+    debug("Snaplen %u", header.snaplen);
     debug("Linktype %u", header.linktype);
     debug("PCAP version %hu.%hu", header.version_major, header.version_minor);
     debug("PCAP thiszone %d", header.thiszone);
+#endif
+}
 
-    // Prepare buffer
-    buffer_.resize(header.snaplen);
+inline void Reader::fillBuffer()
+{
+    auto data = buffer_.data();
+    std::size_t bufferedSize = (end_ - begin_);
+    if (bufferedSize > 0) {
+        std::memmove(data, begin_, bufferedSize);
+    }
+
+    auto bytesToRead = buffer_.size() - bufferedSize;
+    auto bytesRead = file_.read(buffer_.data() + bufferedSize, bytesToRead);
+    begin_ = data;
+    end_ = data + bytesRead + bufferedSize;
+}
+
+inline std::size_t Reader::read(void*& buffer, std::size_t size)
+{
+    std::size_t bufferedSize = (end_ - begin_);
+    if (NETBOX_UNLIKELY(bufferedSize < size)) {
+        fillBuffer();
+        bufferedSize = (end_ - begin_);
+    }
+
+    auto sizeToCopy = std::min(bufferedSize, size);
+    buffer = begin_;
+    begin_ += sizeToCopy;
+    return sizeToCopy;
 }
 
 } /* namespace netbox::pcap */
